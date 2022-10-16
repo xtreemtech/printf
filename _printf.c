a606693d656ecef205152dc20b81083da2ddf8bb
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-10-16 22:28:55 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,92 +1,45 @@
+#include <unistd.h>
 #include "holberton.h"
-#include <stdlib.h>
-#include <stdio.h>
-
 /**
- * printIdentifiers - prints special characters
- * @next: character after the %
- * @arg: argument for the indentifier
- * Return: the number of characters printed
- * (excluding the null byte used to end output to strings)
+ *_printf - takes in a string and prints different types of arguments for
+ * an unspecified amount of arguments
+ * @format: the initial string that tell us what is going to be printed
+ * Return: the amount of times we write to stdout
  */
-
-int printIdentifiers(char next, va_list arg)
-{
-	int functsIndex;
-
-	identifierStruct functs[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"d", print_int},
-		{"i", print_int},
-		{"u", print_unsigned},
-		{"b", print_unsignedToBinary},
-		{"o", print_oct},
-		{"x", print_hex},
-		{"X", print_HEX},
-		{"S", print_STR},
-		{NULL, NULL}
-	};
-
-	for (functsIndex = 0; functs[functsIndex].indentifier != NULL; functsIndex++)
-	{
-		if (functs[functsIndex].indentifier[0] == next)
-			return (functs[functsIndex].printer(arg));
-	}
-	return (0);
-}
-
-/**
- * _printf - mimic printf from stdio
- * Description: produces output according to a format
- * write output to stdout, the standard output stream
- * @format: character string composed of zero or more directives
- *
- * Return: the number of characters printed
- * (excluding the null byte used to end output to strings)
- * return -1 for incomplete identifier error
- */
-
 int _printf(const char *format, ...)
 {
-	unsigned int i;
-	int identifierPrinted = 0, charPrinted = 0;
-	va_list arg;
+int i, count;
 
-	va_start(arg, format);
-	if (format == NULL)
-		return (-1);
+int (*f)(va_list);
 
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			charPrinted++;
-			continue;
-		}
-		if (format[i + 1] == '%')
-		{
-			_putchar('%');
-			charPrinted++;
-			i++;
-			continue;
-		}
-		if (format[i + 1] == '\0')
-			return (-1);
+va_list list;
 
-		identifierPrinted = printIdentifiers(format[i + 1], arg);
-		if (identifierPrinted == -1 || identifierPrinted != 0)
-			i++;
-		if (identifierPrinted > 0)
-			charPrinted += identifierPrinted;
+if (format == NULL)
+return (-1);
 
-		if (identifierPrinted == 0)
-		{
-			_putchar('%');
-			charPrinted++;
-		}
-	}
-	va_end(arg);
-	return (charPrinted);
+va_start(list, format);
+i = count = 0;
+
+while (format[i] != '\0')
+{
+if (format[i] == '%')
+{
+if (format[i + 1] == '\0')
+return (-1);
+f = get_func(format[i + 1]);
+if (f == NULL)
+count += print_nan(format[i], format[i + 1]);
+else
+count += f(list);
+i++;
+}
+else
+{
+_putchar(format[i]);
+count++;
+}
+i++;
+}
+va_end(list);
+return (count);
 }
