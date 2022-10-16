e3194d94bc8271bf1c07b92332e16869e7f2368e
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-10-16 13:08:15 +0100

    Update _puts.c

--- _puts.c
+++ _puts.c
@@ -1,1 +1,22 @@
-C - printf
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * new line, to stdout.
+ * @string: input string
+ * Return: count of string.
+ */
+int _puts(char *string)
+{
+	int contador = 0;
+
+	while (*string)
+	{
+		_putchar(*string);
+		string++;
+		contador++;
+	}
+	return (contador);
+}
+Footer
+© 2022 GitH
