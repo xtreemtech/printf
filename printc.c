b06d35cded1b99c2b8e3f33520241f41656c7811
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-10-16 12:50:43 +0100

    Update printc.c

--- printc.c
+++ printc.c
@@ -1,1 +1,14 @@
-C - printf
+#include "main.h"
+
+/**
+ * printc - prints a char.
+ * @pa: format of char.
+ * @format: format
+ * Return: number char for printf.
+ */
+int printc(char *format, va_list pa)
+{
+	(void)format;
+	_putchar(va_arg(pa, int));
+	return (1);
+}
