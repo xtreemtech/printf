055eb32d6a5b7678702309aeb902209cfba8a717
Tajir-Ramadhan <tajiriramadhan@gmail.com>
2022-10-17 14:01:16 +0300

    update on printf programs

--- functions.c
+++ functions.c
@@ -156,7 +156,7 @@ int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	unsigned int n, m, i, sum;
-	unsigned int a[32];
+	unsigned int a[30];
 	int count;
 
 	UNUSED(buffer);
@@ -168,15 +168,15 @@ int print_binary(va_list types, char buffer[],
 	n = va_arg(types, unsigned int);
 	m = 2147483648; /* (2 ^ 31) */
 	a[0] = n / m;
-	for (i = 1; i < 32; i++)
+	for (i = 1; i < 30; i++)
 	{
 		m /= 2;
 		a[i] = (n / m) % 2;
 	}
-	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	for (i = 0, sum = 0, count = 0; i < 30; i++)
 	{
 		sum += a[i];
-		if (sum || i == 31)
+		if (sum || i == 29)
 		{
 			char z = '0' + a[i];
 
