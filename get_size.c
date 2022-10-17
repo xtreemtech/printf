c3ad50a40b3c2654bc83f099be2bd988c24dd5ef
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-10-17 15:36:19 +0100

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- * initialize and declare function
  * get_size - Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
