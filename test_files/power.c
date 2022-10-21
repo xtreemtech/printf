3ccb98d17ec28430afb0681a446a3e0746637f29
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-10-21 02:12:51 +0100

    Update power.c

--- test_files/power.c
+++ test_files/power.c
@@ -1,1 +1,10 @@
-C - Singly linked lists
+long long int power(int base, int x){
+    
+    int result=1;
+    
+    for (x; x>0; x--)
+    {
+    result = result * base;
+    }
+    return (result);
+}
